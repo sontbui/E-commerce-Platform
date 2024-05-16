6e665be71b5e28d3eac8aa3b5d66ccf0870cda39
PhanHuuThinh <thinhphan1171@gmail.com>
2024-05-17 00:51:54 +0700

    update 16/5

--- shopapp-backend/src/test/java/com/project/shopapp/TestLogin.java
+++ shopapp-backend/src/test/java/com/project/shopapp/TestLogin.java
@@ -43,115 +43,71 @@ public class TestLogin {
 		Thread.sleep(500);
 		driver.findElement(new By.ByClassName("login-button")).click();
 		Thread.sleep(500);
-
-		//thao tác thêm một category
-		driver.findElement(By.id("test_categories")).click();
-		Thread.sleep(500);
-		driver.findElement(new By.ByClassName("btn-success")).click();
-		driver.findElement(By.id("name")).sendKeys("mouse");
-		Thread.sleep(1000);
-		driver.findElement(new By.ByClassName("btn-primary")).click();
-		driver.navigate().back();
-		Thread.sleep(1000);
-
-
-		//thao tác tìm sản phẩm trong product
-		driver.findElement(By.id("test_product")).click();
-		driver.findElement(new By.ByClassName("form-control")).sendKeys("Tivi");
-		driver.findElement(By.id("btn-find")).click();
-		Thread.sleep(1000);
-		driver.findElement(new By.ByClassName("btn-primary")).click();
-		Thread.sleep(1000);
-
-		//thao tác thêm sản phẩm trong product
-		driver.findElement(By.id("btn-add")).click();
-		driver.findElement(By.id("name")).sendKeys("test");
-		driver.findElement(By.id("price")).sendKeys("10");
-		driver.findElement(By.id("description")).sendKeys("test description");
-
-		WebElement userDropdown = driver.findElement(By.id("category"));
-		// Khởi tạo Select object với WebElement
-		Select dropdown = new Select(userDropdown);
-		dropdown.selectByIndex(0);
-		Thread.sleep(1000);
-
-		//WebElement fileInput = driver.findElement(By.id("images"));
-		// chưa accept được path ảnh
-		//fileInput.sendKeys("C:\Users\Admin\OneDrive\Hình ảnh\received_5645669542185850.jpg");
-
-
-
-		//thao tác quản lý user
-		driver.findElement(By.id("test_user")).click();
-		Thread.sleep(1000);
-		driver.findElement(new By.ByClassName("btn-danger")).click();
-		Thread.sleep(1000);
-
 	}
 
 	//Sign in for user
-//	@Test
-//	public void TestCase2()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("123123");
-//		driver.findElement(By.name("password")).sendKeys("123123");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//	//Null phone and password
-//	@Test
-//	public void TestCase3()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("");
-//		driver.findElement(By.name("password")).sendKeys("");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//
-//	//Null phone, correct password
-//	@Test
-//	public void TestCase4()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("");
-//		driver.findElement(By.name("password")).sendKeys("123123");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//
-//	//correct phone, null password
-//	@Test
-//	public void TestCase5()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("123123");
-//		driver.findElement(By.name("password")).sendKeys("");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//
-//	//correct phone, wrong password
-//	@Test
-//	public void TestCase6()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("123123");
-//		driver.findElement(By.name("password")).sendKeys("hhhhh");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//
-//
-//	//wrong phone, correct password
-//	@Test
-//	public void TestCase7()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("111111111111111");
-//		driver.findElement(By.name("password")).sendKeys("123123");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
-//
-//
-//	//wrong phone, wrong password
-//	@Test
-//	public void TestCase9()
-//	{
-//		driver.findElement(By.name("phone")).sendKeys("1111111111");
-//		driver.findElement(By.name("password")).sendKeys("55555555");
-//		driver.findElement(new By.ByClassName("login-button")).click();
-//	}
+	@Test
+	public void TestCase2()
+	{
+		driver.findElement(By.name("phone")).sendKeys("123123");
+		driver.findElement(By.name("password")).sendKeys("123123");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+	//Null phone and password
+	@Test
+	public void TestCase3()
+	{
+		driver.findElement(By.name("phone")).sendKeys("");
+		driver.findElement(By.name("password")).sendKeys("");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+
+	//Null phone, correct password
+	@Test
+	public void TestCase4()
+	{
+		driver.findElement(By.name("phone")).sendKeys("");
+		driver.findElement(By.name("password")).sendKeys("123123");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+
+	//correct phone, null password
+	@Test
+	public void TestCase5()
+	{
+		driver.findElement(By.name("phone")).sendKeys("123123");
+		driver.findElement(By.name("password")).sendKeys("");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+
+	//correct phone, wrong password
+	@Test
+	public void TestCase6()
+	{
+		driver.findElement(By.name("phone")).sendKeys("123123");
+		driver.findElement(By.name("password")).sendKeys("hhhhh");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+
+
+	//wrong phone, correct password
+	@Test
+	public void TestCase7()
+	{
+		driver.findElement(By.name("phone")).sendKeys("111111111111111");
+		driver.findElement(By.name("password")).sendKeys("123123");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
+
+
+	//wrong phone, wrong password
+	@Test
+	public void TestCase9()
+	{
+		driver.findElement(By.name("phone")).sendKeys("1111111111");
+		driver.findElement(By.name("password")).sendKeys("55555555");
+		driver.findElement(new By.ByClassName("login-button")).click();
+	}
 
 
 
@@ -161,8 +117,6 @@ public class TestLogin {
 			driver.quit();
 		}
 	}
-
-
 
 }
 
